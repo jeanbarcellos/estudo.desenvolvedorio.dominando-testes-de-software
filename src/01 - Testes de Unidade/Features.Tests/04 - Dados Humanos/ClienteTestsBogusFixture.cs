8b4e3a38da60b32d924342fd225e8106d89f7d47
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-15 22:15:48 -0300

    03-10 - Gerando dados humanos

---
+++ src/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>
+    {}
+
+    public class ClienteTestsBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            //var email = new Faker().Internet.Email("eduardo","pires","gmail");
+            //var clientefaker = new Faker<Cliente>();
+            //clientefaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80,DateTime.Now.AddYears(-18)),
+                    "",
+                    true,
+                    DateTime.Now))
+                .RuleFor(c=>c.Email, (f,c) =>
+                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(1, DateTime.Now.AddYears(1)),
+                    "",
+                    false,
+                    DateTime.Now));
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
