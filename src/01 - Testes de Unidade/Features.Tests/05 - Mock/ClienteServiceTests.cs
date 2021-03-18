5d6c293743dccf7f8765a842487805157a8b7946
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-18 20:46:23 -0300

    03-11 - Realizando mock de objetos

---
+++ src/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading;
+using Features.Clientes;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        readonly ClienteTestsBogusFixture _clienteTestsBogus;
+
+        public ClienteServiceTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsBogus = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            Assert.True(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente),Times.Once);
+            mediatr.Verify(m=>m.Publish(It.IsAny<INotification>(),CancellationToken.None),Times.Once);
+        }
+
+    }
+}
