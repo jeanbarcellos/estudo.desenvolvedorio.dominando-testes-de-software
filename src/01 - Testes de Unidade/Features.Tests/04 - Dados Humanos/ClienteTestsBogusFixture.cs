b9442671f1b0ee854dec52c4e40009579e94c7e9
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-18 21:18:13 -0300

    03-12 - Simulando retorno de métodos com Mock

--- src/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
+++ src/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Bogus;
 using Bogus.DataSets;
 using Features.Clientes;
@@ -8,11 +10,26 @@ namespace Features.Tests
 {
     [CollectionDefinition(nameof(ClienteBogusCollection))]
     public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>
-    {}
+    { }
 
     public class ClienteTestsBogusFixture : IDisposable
     {
         public Cliente GerarClienteValido()
+        {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
         {
             var genero = new Faker().PickRandom<Name.Gender>();
 
@@ -20,19 +37,19 @@ namespace Features.Tests
             //var clientefaker = new Faker<Cliente>();
             //clientefaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
 
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
-                    f.Date.Past(80,DateTime.Now.AddYears(-18)),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     "",
-                    true,
+                    ativo,
                     DateTime.Now))
-                .RuleFor(c=>c.Email, (f,c) =>
-                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+                .RuleFor(c => c.Email, (f, c) =>
+                      f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInvalido()
