0f585724e9301807d26869512c15c52d2db1379f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-21 18:04:40 -0300

    04-05 - Pedido - Adicionar Item - Parte 1

---
+++ src/02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public decimal ValorTotal { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItems.Add(pedidoItem);
+            ValorTotal = PedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+        }
+    }
+}
