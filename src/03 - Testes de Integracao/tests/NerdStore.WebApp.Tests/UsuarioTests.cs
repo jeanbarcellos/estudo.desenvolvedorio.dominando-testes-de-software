cde29e1e727dfaa663056bbaf50593ed81fd2ee4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-27 18:00:24 -0300

    05-04 - Registro do usuário - p1

--- src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -1,5 +1,7 @@
 ﻿using NerdStore.WebApp.MVC;
 using NerdStore.WebApp.Tests.Config;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -19,14 +21,33 @@ namespace NerdStore.WebApp.Tests
         [Trait("Categoria", "Integração Web - Usuário")]
         public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
         {
-            //// Arrange
+            // Arrange
             var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
             initialResponse.EnsureSuccessStatusCode();
 
+            var email = "teste2@teste.com";
+
+            var formData = new Dictionary<string, string>
+            {
+                {"Input.Email", email},
+                {"Input.Password", "Teste@123"},
+                {"Input.ConfirmPassword", "Teste@123"}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+
             // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
 
             // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
 
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {email}!", responseString);
         }
 
     }
