3d0534075a0b1199260aa06440d9fc041d45ee48
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-27 18:32:50 -0300

    05-06 - Registro do usuário - pt03 - Parte 2 Criação do teste: Realizar Login

--- src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -89,5 +89,37 @@ namespace NerdStore.WebApp.Tests
             Assert.Contains("Passwords must have at least one lowercase (&#x27;a&#x27;-&#x27;z&#x27;).", responseString);
             Assert.Contains("Passwords must have at least one uppercase (&#x27;A&#x27;-&#x27;Z&#x27;).", responseString);
         }
+
+        [Fact(DisplayName = "Realizar login com sucesso")]
+        [Trait("Categoria", "Integração Web - Usuário")]
+        public async Task Usuario_RealizarLogin_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Login");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>
+            {
+                {_testsFixture.AntiForgeryFieldName, antiForgeryToken},
+                {"Input.Email", _testsFixture.UsuarioEmail},
+                {"Input.Password", _testsFixture.UsuarioSenha}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {_testsFixture.UsuarioEmail}!", responseString);
+        }
     }
 }
