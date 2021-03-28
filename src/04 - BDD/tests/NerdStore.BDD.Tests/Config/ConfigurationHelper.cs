8c5e677940d88a9d51cf3ce9174f6cc1cf135bce
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-28 20:22:59 -0300

    07-04 - Customizando o uso do Selenium Finalização da configuração base dos arquivos

--- src/04 - BDD/tests/NerdStore.BDD.Tests/Config/ConfigurationHelper.cs
+++ src/04 - BDD/tests/NerdStore.BDD.Tests/Config/ConfigurationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace NerdStore.BDD.Tests.Config
 {
@@ -14,5 +15,15 @@ namespace NerdStore.BDD.Tests.Config
         }
 
         public string WebDrivers => $"{_config.GetSection("WebDrivers").Value}";
+
+        public string FolderPath => Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+        public string FolderPicture => $"{FolderPath}{_config.GetSection("FolderPicture").Value}";
+
+        public string VitrineUrl => _config.GetSection("VitrineUrl").Value;
+        public string ProdutoUrl => $"{DomainUrl}{_config.GetSection("ProdutoUrl").Value}";
+        public string CarrinhoUrl => $"{DomainUrl}{_config.GetSection("CarrinhoUrl").Value}";
+        public string DomainUrl => _config.GetSection("DomainUrl").Value;
+        public string RegisterUrl => $"{DomainUrl}{_config.GetSection("RegisterUrl").Value}";
+        public string LoginUrl => $"{DomainUrl}{_config.GetSection("LoginUrl").Value}";
     }
 }
