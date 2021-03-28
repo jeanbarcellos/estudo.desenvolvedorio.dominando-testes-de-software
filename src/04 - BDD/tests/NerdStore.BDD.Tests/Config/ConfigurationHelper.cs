f7cb2ed19c456dcc51c8d13159e80fa9f68bd9be
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-28 20:12:10 -0300

    07-03 - Configurando o Selenium da forma certa

--- src/04 - BDD/tests/NerdStore.BDD.Tests/Config/ConfigurationHelper.cs
+++ src/04 - BDD/tests/NerdStore.BDD.Tests/Config/ConfigurationHelper.cs
@@ -1,6 +1,18 @@
-﻿namespace NerdStore.BDD.Tests.Config
+﻿using Microsoft.Extensions.Configuration;
+
+namespace NerdStore.BDD.Tests.Config
 {
     public class ConfigurationHelper
     {
+        private readonly IConfiguration _config;
+
+        public ConfigurationHelper()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public string WebDrivers => $"{_config.GetSection("WebDrivers").Value}";
     }
 }
--- src/04 - BDD/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
+++ src/04 - BDD/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -1,6 +1,44 @@
-﻿namespace NerdStore.BDD.Tests.Config
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace NerdStore.BDD.Tests.Config
 {
-    public class SeleniumHelper
+    // Abstração do WebDriver
+    public class SeleniumHelper : IDisposable
     {
+        public IWebDriver WebDriver;
+        public readonly ConfigurationHelper Configuration;
+        public WebDriverWait Wait;
+
+        public SeleniumHelper(Browser browser, ConfigurationHelper configuration, bool headless = true)
+        {
+            Configuration = configuration;
+            WebDriver = WebDriverFactory.CreateWebDriver(browser, Configuration.WebDrivers, headless);
+            WebDriver.Manage().Window.Maximize();
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(30));
+        }
+
+        public string ObterUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void IrParaUrl(string url)
+        {
+            WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public void ClicarLinkPorTexto(string linkText)
+        {
+            var link = Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText(linkText)));
+            link.Click();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
